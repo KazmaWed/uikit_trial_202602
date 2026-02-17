c2ccc6eb640ff4d9e7513abfd150ab447671ea8d
kazmawed <harinezumi.camera@gmail.com>
2026-02-17 21:04:27 +0900

    Initial Commit

---
+++ uikit_trial_202602/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  uikit_trial_202602
+//
+//  Created by Kazma Wed on 2026/02/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
